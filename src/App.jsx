b256a4f8441a8c07effbfb5c5b314318b34dc422
MicalBelo <ribeiro.mical@escolar.ifrn.edu.br>
2025-06-14 02:36:57 +0000

    Atividade Avaliativa

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { MyGrid } from "./components/MyGrid";
+
+import { Footer } from "./components/Footer"
+import { Main } from "./components/Main"
+import { Header } from "./components/Header"
 
 export default function App() {
 
   return (
     //React Fragment
     <>
-      <MyGrid />
+      <Header />
+      <Main />
+      <Footer />
+
     </>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,1 +1,40 @@
 import styles from "./Main.module.css";
+import img from "./assets/img/wolv.jpg";
+export function Main() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header} />
+      <aside className={styles.aside} />
+      <div className={styles.main}>
+        <div className={styles.grid}>
+          <div className={styles.card}>
+         <img src="https://picsum.photos/200?random=2"/>
+            <h2>Card 1</h2>
+            <p>Ele é fofo</p>
+          </div>
+          <div className={styles.card}>
+            <img src="https://picsum.photos/200?random=2"/>
+            <h2>Card 2</h2>
+            <p>Ele é engraçado</p>
+          </div>
+          <div className={styles.card}>
+            <img src="https://picsum.photos/200?random=3"/>
+            <h2>Card 3</h2>
+            <p>Ele é romantico</p>
+          </div>
+          <div className={styles.card}>
+            <img src="https://picsum.photos/200?random=4"/>
+            <h2>Card 4</h2>
+            <p>Ele é meio agressivo</p>
+          </div>
+          <div className={styles.card}>
+            <img src="https://picsum.photos/200?random=5"/>
+            <h2>Card 5</h2>
+            <p>Mas tambem pode ser muito amoroso</p>
+          </div>
+        </div>
+      </div>
+      <footer className={styles.footer} />
+    </div>
+  );
+}
