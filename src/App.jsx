0a78ac3841383dbf48287f3d2531bd467a04c6ce
MicalBelo <ribeiro.mical@escolar.ifrn.edu.br>
2025-06-27 00:27:54 +0000

    Desafio 04

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-
-// import { MyGrid } from "./components/MyGrid";
-// import { Footer } from "./components/Footer"
-// import { Main } from "./components/Main"
-import { Header } from "./components/Etapa1/Header"
-import { LuckyNumber } from "./components/Etapa2/LuckyNumber";
+import { Header } from "./components/etapa1/Header";
+import { LuckyNumber } from "./components/etapa2/LuckyNumber";
 
 export default function App() {
 
   return (
     //React Fragment
     <>
-
       <Header />
       <LuckyNumber />
     </>
--- src/components/etapa1/Header.jsx
+++ src/components/etapa1/Header.jsx
@@ -1,18 +1,16 @@
 import styles from "./Header.module.css";
 import img from "../assets/img/deade.jpg";
 
-
 export function Header() {
   return (
-
-    <div className={styles.centralContainer}>
+    <div className={styles.container}>
       <div className={styles.containerImg}>
-        <img src={img}
-          alt="dead"
+        <img
+          src={img}
+          alt="Foco, Força e Fé"
         />
       </div>
-      <h1 className={styles.text}>Lets Go!</h1>
+      <h1>Foco, Força, Fé</h1>
     </div>
-
   );
 }
